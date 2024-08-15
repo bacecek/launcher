8f184f7892d8467e2bb1e1479b76acbf5543d51d
Denis Buzmakov <me@bacecek.dev>
2024-08-15 21:06:21 +0200

    Update compile and target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "dev.bacecek.launcher"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.bacecek.launcher"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
